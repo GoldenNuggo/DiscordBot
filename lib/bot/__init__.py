dbd1b9dfbaacf7846c92094e6d99405cfddeb671
Goldennuggo <91820572+GoldenNuggo@users.noreply.github.com>
2022-05-20 10:21:48 +0000

    - Added like 200 files - Mainly rewritten - v0.0.1

---
+++ lib/bot/__init__.py
@@ -0,0 +1,43 @@
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from discord.ext.commands import Bot as BotBase
+
+PREFIX = "!"
+OWNER_ID = [628480432467607552]
+
+class Bot(BotBase):
+  def __init__(self):
+    self.PREFIX = PREFIX
+    self.ready = False
+    self.guild = None # Remove later if going to multiple servers
+    self.scheduler = AsyncIOScheduler()
+    
+    super().__init__(command_prefix=PREFIX, owner_ids=OWNER_ID)
+    
+  def run(self, version):
+    self.VERSION = version
+
+    with open ("./lib/bot/token.0", "r", encoding="utf-8") as tf:
+      self.TOKEN = tf.read()
+
+      print("Running Bot...")
+      super().run(self.TOKEN, reconnect=True)
+
+  async def on_connect(self):
+    print("Bot Connected")
+
+  async def on_disconnect(self):
+    print("Bot Disconnected")
+
+  async def on_ready(self):
+    if not self.ready:
+      self.ready = True
+      self.guild = self.get_guild(852117164416958514)
+      print("Bot Ready")
+
+    else:
+      print("Bot Reconnected")
+
+  async def on_mesage(self, message):
+    pass
+
+bot = Bot()
